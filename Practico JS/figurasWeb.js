d59327415aaae0165c3bacc119103b2f11ef50d6
ManuelVoiden <jmmb710@hotmail.com>
2022-05-31 03:33:06 -0500

    Added rounding to the results

--- Practico JS/figurasWeb.js
+++ Practico JS/figurasWeb.js
@@ -16,7 +16,7 @@ function calculoTriangle(){ //Calcula la area y perimetro de un triangulo
     let triangleSideB = parseInt(document.getElementById("triangleSideB").value);
     let trianglePerimeter = triangleSideA + triangleSideB + triangleBase;
     //return [triangleArea, trianglePerimeter];
-    alert("triangleArea: " + triangleArea + " trianglePerimeter: " + trianglePerimeter);
+    alert("triangleArea: " + round(triangleArea,2) + " trianglePerimeter: " + round(trianglePerimeter,2));
     event.preventDefault();
 }
 
@@ -25,7 +25,7 @@ function calculoSquare(){ //Calcula la area y perimetro de un cuadrado
     let squareArea = (squareSide * squareSide);
     let squearePerimeter = (squareSide * 4);
     //return [squareArea, squearePerimeter];
-    alert("squareArea: " + squareArea + " squearePerimeter: " + squearePerimeter);
+    alert("squareArea: " + round(squareArea,2) + " squearePerimeter: " + round(squearePerimeter,2));
     event.preventDefault();
 }
 
@@ -35,7 +35,7 @@ function calculoRectangle(){ //Calcula el area y perimetro de un rectangulo
     let rectangleArea = (rectangleBase * rectangleHeight);
     let rectanglePerimeter = (rectangleBase * 2) + (rectangleHeight * 2);
     //return [rectangleArea, rectanglePerimeter];
-    alert("rectangleArea: " + rectangleArea + " rectanglePerimeter: " + rectanglePerimeter);
+    alert("rectangleArea: " + round(rectangleArea,2) + " rectanglePerimeter: " + round(rectanglePerimeter,2));
     event.preventDefault();
 }
 
@@ -45,8 +45,21 @@ function calculoCircle(){ //Calcula la area y perimetro de un circulo
     let circlePerimeter = (2 * pi * circleRadius);
     let circleDiameter = (2 * circleRadius);
     //return [circleArea, circlePerimeter, circleDiameter];
-    alert("circleArea: " + circleArea + " circlePerimeter: " + circlePerimeter + " circleDiameter: " + circleDiameter);
+    alert("circleArea: " + round(circleArea,2) + " circlePerimeter: " + round(circlePerimeter,2) + " circleDiameter: " + round(circleDiameter,2));
     event.preventDefault();
+}
+
+function round(num, decimales = 2) { //Redondea un numero
+    var signo = (num >= 0 ? 1 : -1);
+    num = num * signo;
+    if (decimales === 0) //con 0 decimales
+        return signo * Math.round(num);
+    // round(x * 10 ^ decimales)
+    num = num.toString().split('e');
+    num = Math.round(+(num[0] + 'e' + (num[1] ? (+num[1] + decimales) : decimales)));
+    // x * 10 ^ (-decimales)
+    num = num.toString().split('e');
+    return signo * (num[0] + 'e' + (num[1] ? (+num[1] - decimales) : -decimales));
 }
 
 //console.log("Cálculo de figuras");
