2c791129a2059eda898ecf19619b7a67bb333758
ManuelVoiden <jmmb710@hotmail.com>
2022-09-27 02:23:17 -0500

    Finished queue structure

--- DataStructures/queue.js
+++ DataStructures/queue.js
@@ -23,4 +23,31 @@ class queue {
     size() {
         return this.length;
     }
+
+    queue() {
+        const newNode = new Node(value);
+        if (this.length === 0) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            const holdingPointer = this.last;
+            this.last = newNode;
+            this.last.next = holdingPointer;
+        }
+        this.length++;
+        return this;
+    }
+
+    dequeue() {
+        if (this.first === this.last) {
+            this.first = null;
+            this.last = null;
+        } else {
+            const holdingPointer = this.first;
+            this.first = this.first.next;
+            this.first.next = null;
+            this.length--;
+            return holdingPointer;
+        }
+    }
 }
