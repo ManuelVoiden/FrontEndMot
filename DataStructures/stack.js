0522186121cc74aa3e128d30d3c1cf4daa7f1c2f
ManuelVoiden <jmmb710@hotmail.com>
2022-09-27 02:15:52 -0500

    Final version of the stack class

--- DataStructures/stack.js
+++ DataStructures/stack.js
@@ -26,12 +26,26 @@ class Stack {
             this.top = newNode;
             this.bottom = newNode;
         } else {
-            const holdingPointer = this.top;
+            const holdingPointer = this.top;  // prevent the node from being destroyed when the garbage collector parse the stack
             this.top = newNode;
             this.top.next = holdingPointer;
         }
         this.length++;
         return this;
+    }
+
+    pop() {
+
+        if (this.top === this.bottom) {
+            this.top = null;
+            this.bottom = null;
+        } else {
+            const holdingPointer = this.top;
+            this.top = this.top.next;
+            this.top.next=null;
+            this.length--;
+            return holdingPointer;
+        }
     }
 
     size() {
@@ -44,7 +58,16 @@ const myStack = new Stack();
 console.log(myStack.push("Google"));
 console.log(myStack.push("Udemy"));
 console.log(myStack.push("Discord"));
+console.log(myStack.push("Platzi"));
+console.log(myStack.push("GitHub"));
+console.log(myStack.push("Facebook"));
+console.log(myStack.push("Instagram"));
+console.log(myStack.push("Twitter"));
+
 
 console.log("At the top of the stack you can find: ", myStack.peek());
 console.log("At the bottom of the stack you can find: ", myStack.lift());
 console.log("The current size of the stack is: ", myStack.size(), "elements");
+console.log(myStack.pop());
+console.log("The current size of the stack is: ", myStack.size(), "elements")
+console.log("At the top of the stack you can find: ", myStack.peek());
